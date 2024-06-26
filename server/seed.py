20762bb802c3daee7a41bfb389ab329395fe121e
Brian <turtle40965@gmail.com>
2024-06-26 15:58:22 -0400

    complete

--- server/seed.py
+++ server/seed.py
@@ -11,15 +11,15 @@ with app.app_context():
     Bakery.query.delete()
     
     bakeries = []
-    bakeries.append(Bakery(name='Delightful donuts'));
-    bakeries.append(Bakery(name='Incredible crullers'));
+    bakeries.append(Bakery(name='Delightful donuts'))
+    bakeries.append(Bakery(name='Incredible crullers'))
     db.session.add_all(bakeries)
 
     baked_goods = []
-    baked_goods.append(BakedGood(name='Chocolate dipped donut', price=2.75, bakery=bakeries[0]));
-    baked_goods.append(BakedGood(name='Apple-spice filled donut', price=3.50, bakery=bakeries[0]));
-    baked_goods.append(BakedGood(name='Glazed honey cruller', price=3.25, bakery=bakeries[1]));
-    baked_goods.append(BakedGood(name='Chocolate cruller', price=3.40, bakery=bakeries[1]));
+    baked_goods.append(BakedGood(name='Chocolate dipped donut', price=2.75, bakery=bakeries[0]))
+    baked_goods.append(BakedGood(name='Apple-spice filled donut', price=3.50, bakery=bakeries[0]))
+    baked_goods.append(BakedGood(name='Glazed honey cruller', price=3.25, bakery=bakeries[1]))
+    baked_goods.append(BakedGood(name='Chocolate cruller', price=3.40, bakery=bakeries[1]))
 
     db.session.add_all(baked_goods)
     db.session.commit()
